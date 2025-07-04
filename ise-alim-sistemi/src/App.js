380bae052341c46ac5e1ac6fa03e6f39dca5b1a0
IlginVural <vuralilgin05@gmail.com>
2025-07-04 12:55:32 +0300

    login deleted
    deleted the login for easy application

--- ise-alim-sistemi/src/App.js
+++ ise-alim-sistemi/src/App.js
@@ -8,9 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        
         <Route path ="/AdminDashboard" element={<AdminDashboard />} />
         <Route path ="/Jobs" element={<Jobs />} />
       </Routes>
