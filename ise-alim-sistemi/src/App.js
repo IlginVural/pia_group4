7aba03dae10da690b87e211d099221d8e4b371fd
IlginVural <vuralilgin05@gmail.com>
2025-07-04 16:31:57 +0300

    register page deleted
    get rid of the registered page

--- ise-alim-sistemi/src/App.js
+++ ise-alim-sistemi/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import Jobs from './pages/admin/Jobs';
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path ="/AdminDashboard" element={<AdminDashboard />} />
         <Route path ="/Jobs" element={<Jobs />} />
